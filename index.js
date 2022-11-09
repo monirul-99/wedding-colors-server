18c7ca9cfb3cf23bd2dd70ed926c3f7936e54e5d
monaulic <sohelmuhammad164@gmail.com>
2022-11-09 16:24:09 +0600

    review delete handle added

--- index.js
+++ index.js
@@ -96,6 +96,14 @@ async function run() {
       console.log(result);
       res.send(result);
     });
+
+    app.delete("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+      console.log(result);
+    });
   } finally {
   }
 }
